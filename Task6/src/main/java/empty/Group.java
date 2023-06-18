b20124540708d5c0731008a7c51c2354eb02d271
VZon9 <mark.bogdanov.2001@gmail.com>
2023-06-18 21:18:19 +0300

    Task6 only home and add group

---
+++ Task6/src/main/java/empty/Group.java
@@ -0,0 +1,34 @@
+package empty;
+
+import java.util.List;
+
+public class Group {
+    private int id;
+    private String name;
+    private List<Album> albumList;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Album> getAlbumList() {
+        return albumList;
+    }
+
+    public void setAlbumList(List<Album> albumList) {
+        this.albumList = albumList;
+    }
+}
---
+++ Task6/src/main/java/empty/Song.java
@@ -0,0 +1,32 @@
+package empty;
+
+public class Song {
+    private int id;
+    private String name;
+    private String duration;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
